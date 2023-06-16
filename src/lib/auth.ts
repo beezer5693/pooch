5ae92e0a22de4403624aa73a750782cdfbf2de2a
Brandon Bryan <bshwag@gmail.com>
2023-06-16 18:13:13 -0400

    Updated next auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -10,9 +10,6 @@ export const authOptions: NextAuthOptions = {
 	session: {
 		strategy: "jwt",
 	},
-	pages: {
-		signIn: "/sign-in",
-	},
 	providers: [
 		GoogleProvider({
 			clientId: process.env.GOOGLE_CLIENT_ID as string,
